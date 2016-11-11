33ecf0c0a9133b4ad3cc7fa6d18d93eca96fe0cf
张琼 <zhangqiong1995@foxmail.com>
2016-11-11 15:19:10 +0800

    move

--- MyPractice/MyPractice/fxq/js/move.js
+++ MyPractice/MyPractice/fxq/js/move.js
@@ -8,8 +8,8 @@ var nums = [];
 
 
 function Parent() {
-    this.isReady = false;
-    this.clickedTimes=0
+    this.isReady = false;   
+    this.onWay = false;
 }
 
 $('.container').click(function () {
@@ -17,24 +17,25 @@ $('.container').click(function () {
         judgeStep(0);
     });
 var colorChoose = function (index, x, y) {
-
     var planes = planeBox[index].children;
      
     for (var i = 0; i < planes.length; i++) {
-        
-         
+               
             planes[i].addEventListener('click', function () {               
                 planes[i]= new Parent();
-                if (isClicked){
-                    planes[i].isReady = true;
-                    planes[i].clickedTimes++;
-                    console.log(planes[i].clickedTimes);
-                    console.log(planes[i].isReady);
+                if (isClicked&&planes[i].isReady==false){
+                    this.isReady = true;          
+                    console.log(this.isReady);
                     this.style.left = x + "px";
                     this.style.top = y + 'px';
                     isClicked = false;
-                }               
+                }
+                else {
+                    
+                }
+               
             });
+        
         }
 
         //replanes[i].removeEventListener('click', startMove);
@@ -43,7 +44,7 @@ var colorChoose = function (index, x, y) {
 
 
 function judgeStep(a){
-    
+   
     junge(a);
     var planes=planeBox[index].children;
     for(var i=0;i<4;i++){
@@ -68,25 +69,26 @@ var firstStep = function () {
 function junge(a) {
     if (nums[a] !== 6) {
         index++;
+        if (index == 4) {
+            index = 0;
+        }
     }
-    if (index == 4) {
-        index = 0;
-    }
+    
     console.log(index);
-    if (index == 0) {
+    if (nums[a]==6&&index == 0) {
         colorChoose(0, 430, 160);
         isClicked = true;
     }
-    else if (index == 1) {
+    else if (nums[a+1] == 6 && index == 1) {
         colorChoose(1, 780, 40);
         isClicked = true;
     }
-    else if (index == 2) {
+    else if (nums[a + 2] == 6 && index == 2) {
         colorChoose(3, 900, 390);
         isClicked = true;
 
     }
-    else if (index == 3) {
+    else if (nums[a + 3] == 6 && index == 3) {
         colorChoose(2, 550, 510);
         isClicked = true;
     }
